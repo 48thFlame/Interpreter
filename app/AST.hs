fde2815ae47f971e17d155d68195b1b70a8e0d1e
48thFlame <79379047+48thFlame@users.noreply.github.com.>
2024-12-25 13:02:01 +0200

    Error handling with "Result" (`Either`)

--- app/AST.hs
+++ app/AST.hs
@@ -10,10 +10,13 @@ import Data.Function ((&))
 import qualified Data.Tree as Tree
 import Data.Tree.Pretty (drawVerticalTree)
 import Debug.Trace
+import Text.Read (readMaybe)
 
 debugLog :: (Show b) => b -> b
 debugLog a =
     traceShow a a
+
+type Result a = Either String a
 
 data Operator
     = Addition
@@ -59,18 +62,20 @@ instance Show AST where
     show ast =
         drawVerticalTree $ astToDataTree ast
 
-stringedTokenToToken :: String -> Token
+stringedTokenToToken :: String -> Result Token
 stringedTokenToToken s =
     case s of
-        "+" -> OpToken Addition
-        "-" -> OpToken Subtraction
-        "*" -> OpToken Multiplication
-        "/" -> OpToken Division
-        "^" -> OpToken Power
-        "(" -> ParToken OpenP
-        ")" -> ParToken CloseP
+        "+" -> Right $ OpToken Addition
+        "-" -> Right $ OpToken Subtraction
+        "*" -> Right $ OpToken Multiplication
+        "/" -> Right $ OpToken Division
+        "^" -> Right $ OpToken Power
+        "(" -> Right $ ParToken OpenP
+        ")" -> Right $ ParToken CloseP
         _ ->
-            NumToken (read s)
+            case readMaybe s of
+                Just n -> Right $ NumToken n
+                Nothing -> Left $ "Invalid token NAN: " ++ s
 
 isNum :: Char -> Bool
 isNum c = isDigit c || c == '.'
@@ -79,38 +84,46 @@ isSpace :: Char -> Bool
 isSpace c = c == ' '
 
 -- | parse a string and return the tokens
-tokenize :: String -> [Token] -> [Char] -> [Token]
+tokenize :: String -> [Token] -> [Char] -> Result [Token]
 -- 3 case where (acc == ""):
 -- done, char is space, start of new token - either number or just operator.
-tokenize "" parsedTokens [] = parsedTokens
+tokenize "" parsedTokens [] = Right parsedTokens
 tokenize "" parsedTokens (char : rest)
     | isSpace char = tokenize "" parsedTokens rest
     | isNum char = tokenize [char] parsedTokens rest
-    | otherwise = tokenize "" (parsedTokens ++ [stringedTokenToToken [char]]) rest
+    | otherwise = case stringedTokenToToken [char] of
+        Right token -> tokenize "" (parsedTokens ++ [token]) rest
+        Left err -> Left err
 -- base case: done going threw the string
 tokenize acc parsedTokens [] =
-    parsedTokens ++ [stringedTokenToToken acc]
+    case stringedTokenToToken acc of
+        Right token -> Right $ parsedTokens ++ [token]
+        Left err -> Left err
 -- otherwise: loop
 tokenize acc parsedTokens (char : rest)
-    | isSpace char = tokenize "" (parsedTokens ++ [stringedTokenToToken acc]) rest
+    | isSpace char = case stringedTokenToToken acc of
+        Right token -> tokenize "" (parsedTokens ++ [token]) rest
+        Left err -> Left err
     -- if isDigit need to assume might be big number: so add to `acc`
     | isNum char = tokenize (acc ++ [char]) parsedTokens rest
-    -- otherwise: must be operator tokenize immediately
+    -- otherwise: must be operator tokenize immediately both `acc` and `char`
     | otherwise =
-        tokenize
-            ""
-            ( parsedTokens
-                ++ [stringedTokenToToken acc, stringedTokenToToken [char]]
-            )
-            rest
+        case (stringedTokenToToken acc, stringedTokenToToken [char]) of
+            (Right token1, Right token2) ->
+                tokenize "" (parsedTokens ++ [token1, token2]) rest
+            (Left err, _) -> Left err
+            (_, Left err) -> Left err
 
-type OpPArStack = [Either Parenthesis Operator]
+{- | OpOrPar is either an operator or a parenthesis,
+used for shunting yard operator stack
+-}
+data OpOrPar = Op Operator | Par Parenthesis
 
-dealWithOperator :: Operator -> OpPArStack -> [AST] -> (OpPArStack, [AST])
-dealWithOperator oppProcessing [] outStack = ([Right oppProcessing], outStack)
+dealWithOperator :: Operator -> [OpOrPar] -> [AST] -> Result ([OpOrPar], [AST])
+dealWithOperator oppProcessing [] outStack = Right ([Op oppProcessing], outStack)
 dealWithOperator oppProcessing oppStack outStack =
     case oppStack of
-        (Right o : restOpp) ->
+        (Op o : restOpp) ->
             if precedence o >= precedence oppProcessing
                 then -- use the operator on top of that stack and loop again (threw the operator stack)
                     case outStack of
@@ -119,27 +132,27 @@ dealWithOperator oppProcessing oppStack outStack =
                                 oppProcessing
                                 restOpp
                                 (AST{left = l, operator = o, right = r} : restOut)
-                        _ -> error "Not enough operands!"
+                        _ -> Left "Not enough operands! (Dealing with operators)"
                 else -- if precedence o < precedence oppProcessing
                 -- add oppProcessing to oppStack
-                    (Right oppProcessing : oppStack, outStack)
-        (Left _ : _) -> (Right oppProcessing : oppStack, outStack)
+                    Right (Op oppProcessing : oppStack, outStack)
+        (Par _ : _) -> Right (Op oppProcessing : oppStack, outStack)
 
-dealWithParenthesis :: OpPArStack -> [AST] -> (OpPArStack, [AST])
-dealWithParenthesis (Left OpenP : rest) outStack = (rest, outStack)
-dealWithParenthesis [] _outStack = error "Unmatched closing operator!"
-dealWithParenthesis (Left CloseP : _) _outStack = error "Dealing with parenthesis, by building ast but hit parenthesis!"
-dealWithParenthesis (Right o : restOpp) outStack =
+dealWithParenthesis :: [OpOrPar] -> [AST] -> Result ([OpOrPar], [AST])
+dealWithParenthesis (Par OpenP : rest) outStack = Right (rest, outStack)
+dealWithParenthesis [] _outStack = Left "Unmatched closing operator!"
+dealWithParenthesis (Par CloseP : _) _outStack = Left "Dealing with parenthesis, by building ast but hit parenthesis!"
+dealWithParenthesis (Op o : restOpp) outStack =
     case outStack of
         (r : l : restOut) ->
             dealWithParenthesis
                 restOpp
                 (AST{left = l, operator = o, right = r} : restOut)
-        _ -> error "Not enough operands!"
+        _ -> Left "Not enough operands! (Dealing with parenthesis)"
 
 -- | shuttingYard :: oppStack outputStack inputTokens -> AST
-shuntingYard :: OpPArStack -> [AST] -> [Token] -> AST
-shuntingYard [] [ast] [] = ast -- finished, done.
+shuntingYard :: [OpOrPar] -> [AST] -> [Token] -> Result AST
+shuntingYard [] [ast] [] = Right ast -- finished, done.
 shuntingYard oppStack outStack (t : rest) =
     -- for each token
     case t of
@@ -148,25 +161,29 @@ shuntingYard oppStack outStack (t : rest) =
             shuntingYard oppStack (Value n : outStack) rest
         -- if it's an OpenParenthesis, just add it to the oppStack
         ParToken OpenP ->
-            shuntingYard (Left OpenP : oppStack) outStack rest
+            shuntingYard (Par OpenP : oppStack) outStack rest
         -- if it's a CloseParenthesis, then `dealWithParenthesis` (keep creating nodes until OpenParenthesis)
         ParToken CloseP ->
-            let (newOppStack, newOutStack) = dealWithParenthesis oppStack outStack
-             in shuntingYard newOppStack newOutStack rest
+            case dealWithParenthesis oppStack outStack of
+                Right (newOppStack, newOutStack) ->
+                    shuntingYard newOppStack newOutStack rest
+                Left err -> Left err
         -- if it's an operator then `dealWithOperator` (check precedence and if needed, create nodes
         OpToken o ->
-            let (newOppStack, newOutStack) = dealWithOperator o oppStack outStack
-             in shuntingYard newOppStack newOutStack rest
+            case dealWithOperator o oppStack outStack of
+                Right (newOppStack, newOutStack) ->
+                    shuntingYard newOppStack newOutStack rest
+                Left err -> Left err
 -- if no tokens left, cleanup.
-shuntingYard (Right o : oppRest) (r : l : outRest) [] =
+shuntingYard (Op o : oppRest) (r : l : outRest) [] =
     shuntingYard oppRest (AST{left = l, operator = o, right = r} : outRest) []
-shuntingYard _ _ _ = error "Invalid input"
+shuntingYard _ _ _ = Left "Failed to parse"
 
 parse :: String -> AST
 parse s =
     tokenize "" [] s
-        & debugLog
-        & shuntingYard [] []
+        & either error (shuntingYard [] [] . debugLog)
+        & either error id
 
 solve :: AST -> Float
 solve (Value n) = n
--- app/Main.hs
+++ app/Main.hs
@@ -9,13 +9,13 @@ main = do
     -- let toParse = "12 *23+  6 ^ 7"
     -- let toParse = "2 * (3-4)"
     -- let toParse = "12 / 0"
-    -- let toParse = "((1+((5 - 4) - 2+2))*(2+(6-3)))"
+    let toParse = "((1+((5 - 4) - 2+2))*(2+(6-3)))"
     -- let toParse = "((5 - - 6))/7"
-    let toParse = "((1)"
+    -- let toParse = "((1+1))"
     print toParse
 
-    let ps = parse toParse
-    print ps
+    let ast = parse toParse
+    print ast
 
-    let solution = solve ps
+    let solution = solve ast
     print solution
